bb7145b84abdabab6ca608ada4e47799b9113b48
Kaio Felipe <kaio.cavalcante.81651@a.fecaf.com.br>
2025-08-27 02:29:18 +0000

    Conectando app.py com o minio.26/08/25 - 23:29

--- DATA_PIPELINE/app.py
+++ DATA_PIPELINE/app.py
@@ -1,8 +1,29 @@
+from minio import Minio
+from minio.error import S3Error
 
-def saudacao():
-    for i in range(5):
-        name = input('What is your name: ')
-        print(f'Welcome Mr.{name}')
+def test_minio():
+    client = Minio(
+        "localhost:8000",
+        access_key="minioadmin",
+        secret_key="minioadmin",
+        secure=False
+    )
 
-if __name__ == '__main__':
-    saudacao()
+    bucket_name = "testando"
+    file_name = "arquivo.txt"
+
+    try:
+        with open(file_name, "w") as f:
+            f.write("Olá, este é um teste de upload para o MinIO!")
+
+        client.fput_object(
+            "testando",
+            "arquivo.txt",
+            file_name
+        )
+        print("Arquivo enviado com sucesso!")
+    except S3Error as e:
+        print(f'Erro ao interagir com o minio: {e}')
+
+if __name__ == "__main__":
+    test_minio()
