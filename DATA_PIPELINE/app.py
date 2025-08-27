9b55c47b9f49cc5188b019e8b82787713e363252
Kaio Felipe <kaio.cavalcante.81651@a.fecaf.com.br>
2025-08-27 03:19:26 +0000

    Conectando o app.py ao minio.27/08/25 - 00:19

--- DATA_PIPELINE/app.py
+++ DATA_PIPELINE/app.py
@@ -13,15 +13,27 @@ def test_minio():
     file_name = "arquivo.txt"
 
     try:
-        with open(file_name, "w") as f:
-            f.write("Olá, este é um teste de upload para o MinIO!")
+        # Criando um arquivo
+        #with open(file_name, "w") as f:
+        #    f.write("Olá, este é um teste de upload para o MinIO!")
 
-        client.fput_object(
-            "testando",
-            "arquivo.txt",
-            file_name
-        )
-        print("Arquivo enviado com sucesso!")
+        # Enviando um arquivo para um bucket específico
+        #client.fput_object(
+        #    "testando",       # Nome do bucket
+        #    "arquivo.txt",    # Nome do arquivo dentro do bucket
+        #    file_name         # Arquivo a ser enviado
+        #)
+
+        # Exibe todos os buckets existentes
+        #buckets = client.list_buckets()
+        #for bucket in buckets:
+        #    print(f'Nome: {bucket.name}, Criado em: {bucket.creation_date}')
+
+        # Exibe os objetos dentro de um bucket específico
+        #objects = client.list_objects("teste", recursive=True)
+        #for object in objects:
+        #    print(object.object_name)
+
     except S3Error as e:
         print(f'Erro ao interagir com o minio: {e}')
 
